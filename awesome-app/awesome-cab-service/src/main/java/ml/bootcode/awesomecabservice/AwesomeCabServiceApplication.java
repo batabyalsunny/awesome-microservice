733dc6b528fa830b6e45e9d993f31bc04775dc5e
Sunny Batabyal <batabyalsunny@gmail.com>
2020-02-21 00:08:34 +0530

    Hystrix and fallback default response added

--- awesome-app/awesome-cab-service/src/main/java/ml/bootcode/awesomecabservice/AwesomeCabServiceApplication.java
+++ awesome-app/awesome-cab-service/src/main/java/ml/bootcode/awesomecabservice/AwesomeCabServiceApplication.java
@@ -2,8 +2,10 @@ package ml.bootcode.awesomecabservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableCircuitBreaker
 @EnableFeignClients
 @SpringBootApplication
 public class AwesomeCabServiceApplication {
--- awesome-app/awesome-cab-service/src/main/java/ml/bootcode/awesomecabservice/controllers/CabOwnerController.java
+++ awesome-app/awesome-cab-service/src/main/java/ml/bootcode/awesomecabservice/controllers/CabOwnerController.java
@@ -4,12 +4,15 @@
 package ml.bootcode.awesomecabservice.controllers;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 import ml.bootcode.dtos.UserDto;
 
@@ -30,6 +33,7 @@ public class CabOwnerController {
 		this.userServiceClient = userServiceClient;
 	}
 
+	@HystrixCommand(fallbackMethod = "usersFallback")
 	@GetMapping("rest")
 	public List<UserDto> getThroughRestTemplate() {
 
@@ -37,9 +41,14 @@ public class CabOwnerController {
 		return Arrays.asList(users);
 	}
 
+	@HystrixCommand(fallbackMethod = "usersFallback")
 	@GetMapping("feign")
 	public List<UserDto> getThroughFeign() {
 
 		return userServiceClient.getUsers();
+	}
+
+	public List<UserDto> usersFallback() {
+		return Collections.emptyList();
 	}
 }
